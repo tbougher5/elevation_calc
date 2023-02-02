778f6ec12617eb6ebeeca43e2fcf7a2695e4a836
tbougher5 <60268319+tbougher5@users.noreply.github.com>
2023-02-02 09:13:37 -0600

    Update streamlit_curtain_wall_calc.py

--- streamlit_curtain_wall_calc.py
+++ streamlit_curtain_wall_calc.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 #import plotly.graph_objects as go
-import pickle
+#import pickle
 #import math
 import matplotlib.pyplot as plt
 #import os
